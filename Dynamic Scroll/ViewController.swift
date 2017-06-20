50101e742a45eee5cdfbb236979b1938c4620aeb
Satish Kumar <satishios25@gmail.com>
2017-06-20 08:19:06 +0530

    First Commit

--- Dynamic Scroll/ViewController.swift
+++ Dynamic Scroll/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mScrollView: UIScrollView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollViewSettings()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func scrollViewSettings() {
+        mScrollView.isScrollEnabled = true
+        mScrollView.isUserInteractionEnabled = true
+        
+        let TotlatX = 10
+        let TotlaY = 2
+        
+       var count = 0
+        var px = 0
+        var py = 0
+        
+        for _ in 1...TotlaY {
+            px = 0
+         
+            for j in 1...TotlatX {
+                count += 1
+                
+                let Button = UIButton()
+                Button.tag = count
+                Button.frame = CGRect(x: px+10, y: py+10, width: 100, height: 45)
+                Button.backgroundColor = UIColor.black
+                Button.setTitle("Hello \(j) ", for: .normal)
+                Button.addTarget(self, action: #selector(scrollButtonAction), for: .touchUpInside)
+                mScrollView.addSubview(Button)
+               px = px + Int(mScrollView.frame.width)/2
+            }
+            py =  Int(mScrollView.frame.height)
+        }
+        
+        mScrollView.contentSize = CGSize(width: px, height: py)
+        
+    }
 
-
+    func scrollButtonAction(sender: UIButton) {
+        print("Hello \(sender.tag) is Selected")
+    }
 }
 
